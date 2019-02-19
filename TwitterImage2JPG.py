9080a5361abeaeb43993e24f5a0709895dcae605
TimiCx <47775109+Timicxx@users.noreply.github.com>
2019-02-19 11:58:23 +0100

    Update TwitterImage2JPG.py

--- TwitterImage2JPG.py
+++ TwitterImage2JPG.py
@@ -1,24 +1,23 @@
 import glob
 import os
+import argparse
 
+parser = argparse.ArgumentParser(description='Converts images with twitter format to jpeg format')
+parser.add_argument('-p', '--path', help="Path to scan for", dest="path", metavar="path", required=True)
+args = vars(parser.parse_args())
 
-def main():
-    os.chdir("F:/Downloads")
-    extensions = ["*.jpg_large", "*.png_large", "*.jpg_orig"]
-    file_list = list()
+os.chdir(args["path"])
+extensions = ["*.jpg_large", "*.png_large", "*.jpg_orig"]
+file_list = list()
 
+for extension in extensions:
+    file_list = file_list + glob.glob(extension)
+
+for file in file_list:
     for extension in extensions:
-        file_list = file_list + glob.glob(extension)
+        new_extension = extension.replace('*', '')
+        if file.endswith(new_extension):
+            new_name = file.replace(new_extension, '') + ".jpg"
+            os.rename(file, new_name)
 
-    for file in file_list:
-        for extension in extensions:
-            new_extension = extension.replace('*', '')
-            if file.endswith(new_extension):
-                new_name = file.replace(new_extension, '') + ".jpg"
-                os.rename(file, new_name)
-
-    print("Done!")
-
-
-if __name__ == __name__:
-    main()
+print("Done!")
