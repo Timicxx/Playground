e6a3c93ffea1017a4395114652d5dc3915f0b60f
TimiCx <47775109+Timicxx@users.noreply.github.com>
2019-02-19 12:01:18 +0100

    Update findPicturesInSubFolders.py

--- findPicturesInSubFolders.py
+++ findPicturesInSubFolders.py
@@ -1,6 +1,12 @@
 import glob
+import argparse
 
-path = ""
+parser = argparse.ArgumentParser(description='Prints all image paths from subfolders in the path specified')
+parser.add_argument('-p', '--path', help="Path to scan for", dest="path", metavar="path", required=True)
+args = vars(parser.parse_args())
+
+
+path = args["path"]
 images = []
 
 image_list = glob.glob(path + "\\*\\*")
